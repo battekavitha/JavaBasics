4ae26d4c6435db875a1bc17ccc39b267d00aaea6
battekavitha <144524167+battekavitha@users.noreply.github.com>
2025-11-21 09:40:29 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan= new Scanner(System.in);
+		String name = scan.nextLine();
+		int age= scan.nextInt();
+		System.out.println("Hello" + name + ", you are "+ age + "years old.");
+		scan.close();
+		
+		
+		
+
+	}
+
+}
